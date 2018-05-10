058ed3a00bdb511cfe6fa4b8d591ec212ffa6b46
16_kulaga <ja.kuba.kulaga@gmail.com>
2018-05-10 18:57:35 +0200

    check1

--- app/web/index.php
+++ app/web/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 0);
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-umask(0002);
+umask(0000);
 
 $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../config/prod.php';
